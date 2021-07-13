efc63720afcc0c2ae0195cab7f7c014b547476a0
daniel-bc <daniel.burtea.codreanu@instana.com>
2021-07-13 14:02:18 +0200

    wip: trying failsafe -> allure

--- src/test/java/com/example/AppIT.java
+++ src/test/java/com/example/AppIT.java
@@ -1,18 +1,60 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.*;
 
 /**
  * Integration test for simple App.
  */
 public class AppIT {
+    Random rand = new Random();
 
     @Test
     public void integrationTestShouldAnswerWithTrue()
     {
         assertTrue( true );
         System.out.println("Integration test passed!");
+    }
+
+    @Test
+    public void dummyIntegrationTestFail()
+    {
+        fail();
+        System.out.println("Integration test failed!");
+    }
+
+    @Test
+    public void randomNumberIsEven()
+    {
+        assertEquals(0, rand.nextInt(100) % 2);
+        System.out.println("Integration test failed!");
+    }
+
+    @Ignore
+    @Test
+    public void randomNumberIsOdd()
+    {
+        assertEquals(1, rand.nextInt(100) % 2);
+        System.out.println("Integration test failed!");
+    }
+
+
+    @Test
+    public void randomNumberIsMultipleOfThree()
+    {
+        assertEquals(0, rand.nextInt(100) % 3);
+        System.out.println("Integration test failed!");
+    }
+
+
+    @Test
+    public void randomNumberIsInTheLower20Range()
+    {
+        assertTrue(rand.nextInt(100) <= 20);
+        System.out.println("Integration test failed!");
     }
 }
